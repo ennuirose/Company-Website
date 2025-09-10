9f72a7a77a465f0047928205478c88494135520f
ennui <ennuiplacebo@protonmail.com>
2025-09-10 18:04:46 +0300

    Vercel fix

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,15 +1,11 @@
 import type { LocalizationResource } from '@clerk/types';
-import type { LocalePrefixMode } from 'next-intl/routing';
 import { enUS } from '@clerk/localizations';
-
-const localePrefix: LocalePrefixMode = 'as-needed';
 
 // FIXME: Update this configuration file based on your project information
 export const AppConfig = {
   name: 'Nextjs Starter',
   locales: ['en'],
   defaultLocale: 'en',
-  localePrefix,
 };
 
 const supportedLocales: Record<string, LocalizationResource> = {
