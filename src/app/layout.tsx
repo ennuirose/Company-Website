319ee10319ef9453cdaa9bfaccf6d5ab12a70d81
ennui <ennuiplacebo@protonmail.com>
2025-09-10 18:55:35 +0300

    bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-gray-950`}>
         <Navbar />
         {children}
         <Footer />
