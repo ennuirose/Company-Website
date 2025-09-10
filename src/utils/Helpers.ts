7d41eb79ea22602728cbe0c974f65b3333d2c229
ennui <ennuiplacebo@protonmail.com>
2025-09-10 17:20:13 +0300

    remove routing

--- src/utils/Helpers.ts
+++ src/utils/Helpers.ts
@@ -1,13 +1,11 @@
-import { routing } from '@/libs/I18nRouting';
-
 export const getBaseUrl = () => {
   if (process.env.NEXT_PUBLIC_APP_URL) {
     return process.env.NEXT_PUBLIC_APP_URL;
   }
 
   if (
-    process.env.VERCEL_ENV === 'production'
-    && process.env.VERCEL_PROJECT_PRODUCTION_URL
+    process.env.VERCEL_ENV === 'production' &&
+    process.env.VERCEL_PROJECT_PRODUCTION_URL
   ) {
     return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
@@ -17,14 +15,6 @@ export const getBaseUrl = () => {
   }
 
   return 'http://localhost:3000';
-};
-
-export const getI18nPath = (url: string, locale: string) => {
-  if (locale === routing.defaultLocale) {
-    return url;
-  }
-
-  return `/${locale}${url}`;
 };
 
 export const isServer = () => {
